53a1755c33a5d590a33e581e90b50039861ac55e
Enijar <TheEnijar@gmail.com>
2026-06-07 14:57:19 +0100

    chore: improve bedmon program

--- src/bedmon.lua
+++ src/bedmon.lua
@@ -1,5 +1,10 @@
 local inv = peripheral.wrap("left")
 local mon = peripheral.find("monitor")
+local modem = peripheral.find("modem")
+
+if modem then
+  rednet.open(peripheral.getName(modem))
+end
 
 if not inv then
   print("No inventory found on left")
@@ -12,12 +17,10 @@ if not mon then
 end
 
 mon.setTextScale(0.5)
-mon.clear()
-mon.setCursorPos(1, 1)
 
 local function writeLine(text)
   local _, y = mon.getCursorPos()
-  local w, h = mon.getSize()
+  local _, h = mon.getSize()
 
   if y > h then
     sleep(3)
@@ -34,6 +37,7 @@ while true do
   mon.setCursorPos(1, 1)
 
   writeLine("Hydroponic Bed Monitor")
+  writeLine("Computer ID: " .. os.getComputerID())
   writeLine("----------------------")
 
   local found = false
